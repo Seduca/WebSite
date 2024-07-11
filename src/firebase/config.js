9d13900aee280e586bd77ab3be91e5e66dc133e7
Your Name <you@example.com>
2024-07-11 17:42:15 -0400

    DC

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -12,15 +12,24 @@ import { initializeApp } from 'firebase/app';
 
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBGuykgxxymY34XJrV32TtraUpYG8EXlB0",
-  authDomain: "logistics-2024-dc168.firebaseapp.com",
-  databaseURL: "https://logistics-2024-dc168-default-rtdb.firebaseio.com",
-  projectId: "logistics-2024-dc168",
-  storageBucket: "logistics-2024-dc168.appspot.com",
-  messagingSenderId: "707217924699",
-  appId: "1:707217924699:web:f39c6017d9977d5187557e"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBGuykgxxymY34XJrV32TtraUpYG8EXlB0",
+//   authDomain: "logistics-2024-dc168.firebaseapp.com",
+//   databaseURL: "https://logistics-2024-dc168-default-rtdb.firebaseio.com",
+//   projectId: "logistics-2024-dc168",
+//   storageBucket: "logistics-2024-dc168.appspot.com",
+//   messagingSenderId: "707217924699",
+//   appId: "1:707217924699:web:f39c6017d9977d5187557e"
+// };
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBAoaeBQY07P4ip2c1iFeHeOmR9Jr61O0Y",
+  authDomain: "seduca-f4e58.firebaseapp.com",
+  databaseURL: "https://seduca-f4e58-default-rtdb.firebaseio.com",
+  projectId: "seduca-f4e58",
+  storageBucket: "seduca-f4e58.appspot.com",
+  messagingSenderId: "654742859778",
+  appId: "1:654742859778:web:8fa7da6919bcd8dd7eb71b"
+};
 
 export const app = initializeApp(firebaseConfig)
